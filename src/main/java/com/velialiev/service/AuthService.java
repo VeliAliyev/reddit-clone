c966765e6c40108432ca0cf183040f90335947a2
VeliAliyev <veli_aliyev@outlook.com>
2022-05-01 15:48:46 +0300

    password encoding implemented

--- src/main/java/com/velialiev/service/AuthService.java
+++ src/main/java/com/velialiev/service/AuthService.java
@@ -2,6 +2,8 @@ package com.velialiev.service;
 
 import com.velialiev.dto.RegisterRequest;
 import com.velialiev.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -9,11 +11,15 @@ import java.time.Instant;
 @Service
 public class AuthService {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void signup(RegisterRequest registerRequest){
         User user = new User();
         user.setUsername(registerRequest.getUsername());
         user.setEmail(registerRequest.getEmail());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setCreatedDate(Instant.now());
+        user.setEnabled(false);
     }
 }
