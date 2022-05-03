011a97e28ef4522f7b4ed564ffec6781ad4e485a
VeliAliyev <veli_aliyev@outlook.com>
2022-05-03 23:34:28 +0300

    logic to retrieve subreddit by id implemented

--- src/main/java/com/velialiev/config/SecurityConfig.java
+++ src/main/java/com/velialiev/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.velialiev.security.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,6 +15,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.annotation.PostConstruct;
 
 @EnableWebSecurity
 @AllArgsConstructor
