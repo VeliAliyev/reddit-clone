bcc0699177321ce53f5d13fe64c6ae7d1f08183d
VeliAliyev <veli_aliyev@outlook.com>
2022-05-04 17:03:08 +0300

    getPost and getAllPosts in PostController and PostService

--- src/main/java/com/velialiev/controller/PostController.java
+++ src/main/java/com/velialiev/controller/PostController.java
@@ -1,14 +1,14 @@
 package com.velialiev.controller;
 
 import com.velialiev.dto.PostRequestDto;
+import com.velialiev.dto.PostResponseDto;
 import com.velialiev.service.PostService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts/")
@@ -20,7 +20,18 @@ public class PostController {
     @PostMapping
     public ResponseEntity createPost(@RequestBody PostRequestDto postRequestDto){
         postService.createPost(postRequestDto);
-        return  ResponseEntity.ok(HttpStatus.OK);
+        return  new ResponseEntity(HttpStatus.CREATED);
     }
 
+    @GetMapping("id")
+    public ResponseEntity<PostResponseDto> getPost(@PathVariable(name = "id") Long id){
+        PostResponseDto postResponseDto = postService.getPost(id);
+        return new ResponseEntity(postResponseDto, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PostResponseDto>> getAllPosts(){
+        List<PostResponseDto> postResponseDtos = postService.getAllPosts();
+        return new ResponseEntity<>(postResponseDtos, HttpStatus.OK);
+    }
 }
--- src/main/java/com/velialiev/service/PostService.java
+++ src/main/java/com/velialiev/service/PostService.java
@@ -1,10 +1,37 @@
 package com.velialiev.service;
 
 import com.velialiev.dto.PostRequestDto;
+import com.velialiev.dto.PostResponseDto;
+import com.velialiev.exceptions.SpringRedditException;
+import com.velialiev.mapper.PostMapper;
+import com.velialiev.model.Post;
+import com.velialiev.repository.PostRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@AllArgsConstructor
 public class PostService {
+
+    final private PostRepository postRepository;
+    final private PostMapper postMapper;
+
     public void createPost(PostRequestDto postRequestDto) {
+        Post post = postMapper.mapDtoToPost(postRequestDto);
+        postRepository.save(post);
+    }
+
+    public PostResponseDto getPost(Long id) {
+        Post post = postRepository.findById(id).orElseThrow(()->new SpringRedditException("No post with such id"));
+        return postMapper.mapPostToDto(post);
+    }
+
+    public List<PostResponseDto> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        List<PostResponseDto> postResponseDtos = posts.stream().map(postMapper::mapPostToDto).collect(Collectors.toList());
+        return postResponseDtos;
     }
 }
