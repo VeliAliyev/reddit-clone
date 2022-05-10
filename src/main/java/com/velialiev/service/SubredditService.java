fe92afa8ae55d393fd1ae3debbc775a8ca820a85
VeliAliyev <veli_aliyev@outlook.com>
2022-05-10 20:56:03 +0300

    Moved mappings to service

--- src/main/java/com/velialiev/service/SubredditService.java
+++ src/main/java/com/velialiev/service/SubredditService.java
@@ -3,7 +3,6 @@ package com.velialiev.service;
 
 import com.velialiev.dto.SubredditDto;
 import com.velialiev.exceptions.SpringRedditException;
-import com.velialiev.mapper.SubredditMapper;
 import com.velialiev.model.SubredditEntity;
 import com.velialiev.repository.SubredditRepository;
 import lombok.AllArgsConstructor;
@@ -11,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,22 +20,41 @@ import java.util.stream.Collectors;
 public class SubredditService {
 
     private final SubredditRepository subredditRepository;
-    private final SubredditMapper subredditMapper;
+
     @Transactional
     public SubredditDto save(SubredditDto subredditDto){
-        SubredditEntity save = subredditRepository.save(subredditMapper.mapDtoToSubreddit(subredditDto));
+        SubredditEntity save = subredditRepository.save(mapDtoToSubreddit(subredditDto));
         subredditDto.setId(save.getSubredditId());
         return subredditDto ;
     }
 
     @Transactional(readOnly = true)
     public List<SubredditDto> getAll() {
-         return subredditRepository.findAll().stream().map(subredditMapper::mapSubredditToDto).collect(Collectors.toList());
+         return subredditRepository.findAll().stream().map(this::mapSubredditToDto).collect(Collectors.toList());
     }
 
     public SubredditDto getSubreddit(Long id) {
-        return subredditMapper.mapSubredditToDto(
+        return mapSubredditToDto(
                 subredditRepository.findById(id)
                         .orElseThrow(()->new SpringRedditException("No subreddit found with id: " + id)));
+    }
+
+    public SubredditDto mapSubredditToDto(SubredditEntity subredditEntity) {
+
+        return SubredditDto.builder()
+                .id(subredditEntity.getSubredditId())
+                .name(subredditEntity.getSubredditName())
+                .description(subredditEntity.getSubredditDescription())
+                .numberOfPosts(subredditEntity.getPostEntities().size())
+                .build();
+    }
+
+    public SubredditEntity mapDtoToSubreddit(SubredditDto subredditDto) {
+        return SubredditEntity.builder()
+                .subredditId(subredditDto.getId())
+                .subredditName(subredditDto.getName())
+                .subredditDescription(subredditDto.getDescription())
+                .createdDate(Instant.now())
+                .build();
     }
 }
