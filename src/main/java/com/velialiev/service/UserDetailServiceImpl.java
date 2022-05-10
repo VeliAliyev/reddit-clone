12c9994eeb613477c1400f3541350b002e0a01eb
VeliAliyev <veli_aliyev@outlook.com>
2022-05-10 20:06:59 +0300

    UserDetailServiceImpl changed to builder

--- src/main/java/com/velialiev/service/UserDetailServiceImpl.java
+++ src/main/java/com/velialiev/service/UserDetailServiceImpl.java
@@ -5,6 +5,7 @@ import com.velialiev.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -26,9 +27,15 @@ public class UserDetailServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
        UserEntity userEntity = userRepository.findByUsername(username).orElseThrow(()->new UsernameNotFoundException("No user with such username"));
-
-        return new org.springframework.security.core.userdetails.User(userEntity.getUsername(), userEntity.getPassword(),
-                userEntity.isEnabled(), true, true, true, getAuthorities("USER"));
+        return User.builder()
+                .username(userEntity.getUsername())
+                .password(userEntity.getPassword())
+                .disabled(false)
+                .accountExpired(false)
+                .credentialsExpired(false)
+                .accountLocked(false)
+                .authorities(getAuthorities("USER"))
+                .build();
     }
 
     private Collection<? extends GrantedAuthority> getAuthorities(String role){
