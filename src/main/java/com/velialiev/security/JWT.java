2581d7e120ed4053827c959ba7326b445c442f65
VeliAliyev <veli_aliyev@outlook.com>
2022-05-10 20:58:18 +0300

    added @Service annotation

--- src/main/java/com/velialiev/security/JWT.java
+++ src/main/java/com/velialiev/security/JWT.java
@@ -10,10 +10,12 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.UUID;
 
+@Service
 @RequiredArgsConstructor
 public class JWT {
 
