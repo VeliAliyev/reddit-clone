706eb958dc62b9f9bb2dccbedf275c00fd57fcef
VeliAliyev <veli_aliyev@outlook.com>
2022-05-10 20:08:16 +0300

    renaming

--- src/main/java/com/velialiev/dto/RefreshAccessTokenRequestDto.java
+++ src/main/java/com/velialiev/dto/RefreshAccessTokenRequestDto.java
@@ -5,12 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthenticationResponse {
-
-    private String authenticationToken;
+public class RefreshAccessTokenRequestDto {
+    @NotBlank
+    private String refreshToken;
     private String username;
-
 }
